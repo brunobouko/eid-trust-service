81d4a3ccdcc49ee843f57d847f16b5b832a3ab6f
frank.cornelis <frank.cornelis@ae532bfc-2517-11de-9078-ef7f9e05793c>
2011-01-06 08:52:37 +0000

    tagging 1.0.0.RC4

---
+++ tags/1.0.0.RC4/eid-trust-service-model/src/main/java/be/fedict/trust/service/dao/AdministratorDAO.java
@@ -0,0 +1,73 @@
+/*
+ * eID Trust Service Project.
+ * Copyright (C) 2009-2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.trust.service.dao;
+
+import java.security.PublicKey;
+import java.security.cert.X509Certificate;
+import java.util.List;
+
+import javax.ejb.Local;
+
+import be.fedict.trust.service.entity.AdminEntity;
+
+/**
+ * Administrator DAO.
+ * 
+ * @author wvdhaute
+ * 
+ */
+@Local
+public interface AdministratorDAO {
+
+	/**
+	 * Returns list of registered administrators.
+	 */
+	List<AdminEntity> listAdmins();
+
+	/**
+	 * Returns {@link AdminEntity} matching the specified {@link PublicKey}.
+	 * Returns <code>null</code> if not found.
+	 * 
+	 * @param publicKey
+	 */
+	AdminEntity findAdmin(PublicKey publicKey);
+
+	/**
+	 * Add new {@link AdminEntity}
+	 * 
+	 * @param authnCertificate
+	 * @param pending
+	 *            pending dministrator or not
+	 */
+	AdminEntity addAdmin(X509Certificate authnCertificate, boolean pending);
+
+	/**
+	 * Remove the specified {@link AdminEntity}.
+	 * 
+	 * @param admin
+	 */
+	void removeAdmin(AdminEntity admin);
+
+	/**
+	 * Return the attached {@link AdminEntity}.
+	 * 
+	 * @param admin
+	 */
+	AdminEntity attachAdmin(AdminEntity admin);
+}
